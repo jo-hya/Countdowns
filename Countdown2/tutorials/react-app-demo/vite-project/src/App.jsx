9d031ed6708b1fef0c0a66e460be4a43aef05da8
jo-hya <137954049+jo-hya@users.noreply.github.com>
2025-04-05 15:20:12 -0400

    file reorganization + trivia project setup

--- Countdown2/tutorials/react-app-demo/vite-project/src/App.jsx
+++ Countdown2/tutorials/react-app-demo/vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BlueButton from './components/BlueButton.jsx'
-import Card from './components/Card'
+import Card from './components/Card.jsx'
 
 
 function App() {
