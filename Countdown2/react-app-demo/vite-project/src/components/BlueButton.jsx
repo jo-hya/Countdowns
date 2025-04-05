d8a37fc2098b31021738c811b9b9eae34e37aebd
jo-hya <137954049+jo-hya@users.noreply.github.com>
2025-04-05 13:42:20 -0400

    react-app-demo

---
+++ Countdown2/react-app-demo/vite-project/src/components/BlueButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/BlueButton.css';
+
+const BlueButton = () => {
+  return (
+    <button className="blue-button">
+      Blue Button
+    </button>
+  );
+};
+
+export default BlueButton;
